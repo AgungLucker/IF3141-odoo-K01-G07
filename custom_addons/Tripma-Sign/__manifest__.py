48f14809b79990145076dcc45b8db8b3eb4eaf0d
agunglucker <iniabi838@gmail.com>
2026-05-12 09:25:57 +0700

    feat: test static odoo

---
+++ custom_addons/Tripma-Sign/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': "TripmaStore",
+    'summary': "Website E-commerce Tripma Sign",
+    'author': "Kelompok 07 - K01",
+    'category': 'Uncategorized',
+    'version': '1.0',
+    'depends': ['base', 'website'], 
+    'data': [
+        'views/templates.xml',
+        'views/menus.xml',
+    ],
+    'application': True,
+}
